3c1620166aa5217de1a03e5ea397f191368b5ebd
Gon4iyKot <lostsoul_d@mail.ru>
2018-05-20 23:08:24 +0400

    hw1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,71 @@
+import com.sun.deploy.util.ArrayUtil;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int counter = 0;
 
     void clear() {
+        storage = new Resume[10000];
+        counter =0;
     }
 
     void save(Resume r) {
+        storage [counter] = r;
+        counter++;
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume fin = null;
+        for(Resume temp:storage) {
+            if(temp!=null) {
+                if (temp.uuid.equals(uuid))
+                    fin = temp;
+            }
+        }
+        return fin;
     }
 
     void delete(String uuid) {
+        for(int i =0; i<storage.length; i++){
+            if (storage[i]!=null){
+                if (storage[i].uuid.equals(uuid)){
+                    storage[i] = null;
+                    break;
+                }
+            }
+        }
+        storage=ShityShifter(storage);
+        counter--;
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] allResume = Arrays.copyOfRange(storage, 0, counter);
+        return allResume;
     }
 
     int size() {
-        return 0;
+        return counter;
+    }
+
+    public Resume[] ShityShifter(Resume[] r) {
+        int mas=0;
+        Resume [] fin = new Resume[r.length];
+        for (int wow=0; wow<r.length;wow++){
+            if (r[wow]!=null) {
+                fin[mas]=r[wow];
+                mas++;
+            }
+        }
+        return fin;
     }
 }
--- src/Resume.java
+++ src/Resume.java
@@ -1,13 +1,28 @@
+import java.util.Comparator;
+
 /**
  * com.urise.webapp.model.Resume class
  */
-public class Resume {
+public class Resume implements Comparator<Resume> {
+    public Resume(String a)
+    {
+        uuid = a;
+    }
 
     // Unique identifier
     String uuid;
 
+    public Resume() {
+
+    }
+
     @Override
+    public int compare(Resume o, Resume o2) {
+        return o2.uuid.compareTo(o.uuid);
+    }
+      @Override
     public String toString() {
         return uuid;
     }
+
 }

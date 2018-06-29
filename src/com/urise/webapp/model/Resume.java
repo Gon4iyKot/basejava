6fa8ebff01b62af50974e28c4d324688f2cd0b80
Gon4iyKot <lostsoul_d@mail.ru>
2018-06-29 10:43:11 +0400

    hw2 v3

---
+++ src/com/urise/webapp/model/Resume.java
@@ -0,0 +1,26 @@
+package com.urise.webapp.model;
+
+/**
+ * com.urise.webapp.model.com.urise.webapp.model.Resume class
+ */
+public class Resume {
+    private String uuid; // Unique identifier
+
+    public Resume(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    @Override
+    public String toString() {
+        return uuid;
+    }
+
+}
--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -1,4 +1,9 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
 import java.util.Arrays;
+
 
 /**
  * Array based storage for Resumes
@@ -7,13 +12,15 @@ public class ArrayStorage {
     private Resume[] storage = new Resume[10000];
     private int counter = 0;
 
-    void clear() {
-        Arrays.fill(storage, 0, counter - 1, null);
-        counter = 0;
+    public void clear() {
+        if (counter != 0) {
+            Arrays.fill(storage, 0, (counter - 1), null);
+            counter = 0;
+        }
     }
 
-    void save(Resume resume) {
-        if (getId(resume.uuid) == null) {
+    public void save(Resume resume) {
+        if (checkIndex(resume.getUuid()) == null) {
             storage[counter] = resume;
             counter++;
         } else {
@@ -21,27 +28,31 @@ public class ArrayStorage {
         }
     }
 
-    void update(String uuid, String uuid2) {
-        if (getId(uuid) != null) {
-            storage[getId(uuid)] = new Resume(uuid2);
+    public void update(String uuid, String uuid2) {
+        Integer indexChecker = checkIndex(uuid);
+        if (indexChecker != null) {
+            storage[indexChecker] = new Resume(uuid2);
         } else {
             System.out.println("Резюме не существует, попробуйте в другой раз");
         }
 
     }
 
-    Resume get(String uuid) {
-        if (getId(uuid) != null) {
-            return storage[getId(uuid)];
+    public Resume get(String uuid) {
+        Integer indexChecker = checkIndex(uuid);
+        if (indexChecker != null) {
+            return storage[indexChecker];
         } else {
             System.out.println("Резюме не существует, попробуйте в другой раз");
             return null;
         }
     }
 
-    void delete(String uuid) {
-        if (getId(uuid) != null) {
-            storage[getId(uuid)] = storage[counter - 1];
+    public void delete(String uuid) {
+        Integer indexChecker = checkIndex(uuid);
+        if (indexChecker != null) {
+            storage[indexChecker] = storage[counter - 1];
+            storage[counter - 1] = null;
             counter--;
         } else {
             System.out.println("Резюме не существует, попробуйте в другой раз");
@@ -51,17 +62,17 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         return Arrays.copyOfRange(storage, 0, counter);
     }
 
-    int size() {
+    public int size() {
         return counter;
     }
 
-    private Integer getId(String id) {
+    private Integer checkIndex(String uuid) {
         for (int i = 0; i < counter; i++) {
-            if (storage[i].uuid.equals(id)) {
+            if (storage[i].getUuid().equals(uuid)) {
                 return i;
             }
         }

b2c1fd7a1700fc00886bff1090862c2dd7c149ec
Gon4iyKot <lostsoul_d@mail.ru>
2018-08-18 14:53:02 +0400

    hw6: AllTests (test in column)

--- test/com/urise/webapp/storage/AllTests.java
+++ test/com/urise/webapp/storage/AllTests.java
@@ -4,7 +4,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ArrayStorageTest.class, SortedArrayStorageTest.class,
-                     MapUuidStorageTest.class, MapStorageTest.class, ListStorageTest.class})
+@Suite.SuiteClasses({ArrayStorageTest.class,
+                     SortedArrayStorageTest.class,
+                     MapUuidStorageTest.class,
+                     MapStorageTest.class,
+                     ListStorageTest.class})
 public class AllTests {
 }

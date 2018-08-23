a32d775d245b6c7825a15b8215af416cf09b9757
Gon4iyKot <lostsoul_d@mail.ru>
2018-08-23 09:55:15 +0400

    hw6: requireNotNull in Resume.class

--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -14,6 +14,8 @@ public class Resume implements Comparable<Resume> {
     }
 
     public Resume(String uuid, String fullName) {
+        Objects.requireNonNull(uuid, "uuid must not be null");
+        Objects.requireNonNull(fullName, "fullName must not be null");
         this.uuid = uuid;
         this.fullName = fullName;
     }
@@ -38,7 +40,7 @@ public class Resume implements Comparable<Resume> {
 
     @Override
     public String toString() {
-        return uuid;
+        return uuid + '(' + fullName + ')';
     }
 
     @Override
--- test/com/urise/webapp/storage/AllTests.java
+++ test/com/urise/webapp/storage/AllTests.java
@@ -4,10 +4,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ArrayStorageTest.class,
-                     SortedArrayStorageTest.class,
-                     MapUuidStorageTest.class,
-                     MapStorageTest.class,
-                     ListStorageTest.class})
+@Suite.SuiteClasses({
+        ArrayStorageTest.class,
+        SortedArrayStorageTest.class,
+        MapUuidStorageTest.class,
+        MapStorageTest.class,
+        ListStorageTest.class})
 public class AllTests {
 }

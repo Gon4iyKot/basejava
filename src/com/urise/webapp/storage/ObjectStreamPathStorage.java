4c7b1d6aca891b18d9b623342b80276106408cdd
Gon4iyKot <lostsoul_d@mail.ru>
2018-09-17 12:06:55 +0400

    hw9: ObjectStreamPathStorage - size() fixed

--- src/com/urise/webapp/storage/ObjectStreamPathStorage.java
+++ src/com/urise/webapp/storage/ObjectStreamPathStorage.java
@@ -37,13 +37,16 @@ public class ObjectStreamPathStorage extends AbstractStorage<Path> {
 
     @Override
     public int size() {
-        return doCopyAll().size();
+        try {
+            return (int) Files.list(directory).count();
+        } catch (IOException e) {
+            throw new StorageException("Directory read error", null);
+        }
     }
 
     @Override
     protected Path getSearchKey(String uuid) {
         return directory.resolve(uuid);
-//        return directory.relativize(Paths.get(uuid));
     }
 
     @Override
